2145de4c230e92787e04a20377039eede6ef0d96
joseph ortiz <joseph.ortiz@careerbuilder.com>
2016-01-09 11:24:02 -0500

    added classes

--- NinjaDomain.Classes/Classes.cs
+++ NinjaDomain.Classes/Classes.cs
@@ -10,7 +10,7 @@ namespace NinjaDomain.Classes
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public bool ServedInOniwaban { get; set; }
+        public bool ServedInOniwaban { get; set; } 
         public Clan Clan { get; set; }
         public int ClanId { get; set; }
         public List<NinjaEquipment> EquipmentOwned { get; set; }
